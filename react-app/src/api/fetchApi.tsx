36e986edccf1b9babcda652e5215fecb555375fe
Sean Yearsley <sean_yearsley_dev@outlook.com>
2023-03-12 21:25:53 +0000

    Tidying up some of the code

--- react-app/src/api/fetchApi.tsx
+++ react-app/src/api/fetchApi.tsx
@@ -1,10 +1,12 @@
 import { JsonConvert } from "../data/jsonConvert";
 import { Title } from "../types/Title";
 
+const apiEndpoint = "https://owfetechtask.blob.core.windows.net/titledata/testdata.json";
+
 /** Function that fetches all of the titles from the API */
 export async function fetchAllTitles(): Promise<Title[]> {
     try {
-        const response = await window.fetch('https://owfetechtask.blob.core.windows.net/titledata/testdata.json');
+        const response = await window.fetch(apiEndpoint);
 
         if (response.ok) {
             const data = await response.text();
@@ -23,11 +25,13 @@ export async function fetchAllTitles(): Promise<Title[]> {
 /** Function that fetches all of the titles from the API and then pulls out the specific title that was passed in via params */
 export async function fetchSpecificTitleDetails(titleNumber: string): Promise<Title | undefined> {
     try {
-        const response = await window.fetch('https://owfetechtask.blob.core.windows.net/titledata/testdata.json');
+        // In an ideal world we would have a different api endpoint that would accept titleNumber as a param which returns
+        // only data for that. This would save us having to pull all the data and filter here in the client.
+        const response = await window.fetch(apiEndpoint);
 
         if (response.ok) {
             const data = await response.text();
-            const titles = JsonConvert.toTitleArrayFromJson(data);
+            const titles = JsonConvert.toTitleArrayFromJson(data);          
             return titles.find(x => x.titleNumber == titleNumber);
         }
         else {
--- react-app/src/components/Shared/ErrorMessage.tsx
+++ react-app/src/components/Shared/ErrorMessage.tsx
@@ -9,7 +9,7 @@ function ErrorMessage({
         <div className="rounded-md bg-rose-300 p-4 border border-rose-500 text-center">
             Sorry! There was an issue processing your request. Please try again later
             {onRetryClickHandler && (
-                <span> (<span className="underline cursor-pointer" onClick={() => onRetryClickHandler()}>retry now</span>)</span>
+                <button className="pl-1" onClick={onRetryClickHandler}>(<span className="underline cursor-pointer">retry now</span>)</button>
             )}
         </div>
     );
